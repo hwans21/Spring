473cf0f6a77e2945663352cf27b3153003f538d6
hwans21 <hwans3747@gmail.com>
2021-07-21 00:16:47 +0900

    생성자 및 setter Spring문법

--- Spring_basic/basic/src/main/java/basic/ex01/MainClass.java
+++ Spring_basic/basic/src/main/java/basic/ex01/MainClass.java
@@ -1,10 +1,17 @@
 package basic.ex01;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
 	public static void main(String[] args) {
-		Chef chef = new Chef();
-		Restaurant res = new Restaurant(chef);
-		Hotel hotel = new Hotel(res);
+//		Chef chef = new Chef();
+//		Restaurant res = new Restaurant(chef);
+//		Hotel hotel = new Hotel(res);
+//		hotel.reserveRestaurant();
+		
+		GenericXmlApplicationContext ct = 
+				new GenericXmlApplicationContext("classpath:test-config.xml");
+		Hotel hotel = ct.getBean("hotel",Hotel.class);
 		hotel.reserveRestaurant();
 	}
 }
