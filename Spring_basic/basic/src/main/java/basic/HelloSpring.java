5d1debe3085c10dde3c35d622e59f5c24ba57fb9
hwans21 <hwans3747@gmail.com>
2021-07-20 22:07:26 +0900

    평소에 자바를 사용할 경우

---
+++ Spring_basic/basic/src/main/java/basic/HelloSpring.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class HelloSpring {
+	public static void main(String[] args) {
+		SpringTest st = new SpringTest();
+		st.hello();
+	}
+}
---
+++ Spring_basic/basic/src/main/java/basic/SpringTest.java
@@ -0,0 +1,7 @@
+package basic;
+
+public class SpringTest {
+	public void hello() {
+		System.out.println("스프링 객체 주입 테스트 : 안녕안녕~");
+	}
+}
