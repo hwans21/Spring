deb4f7b5bd5bd9433f0b289bacaf94f00a9f85a0
hwans21 <hwans3747@gmail.com>
2021-07-21 00:53:37 +0900

    오류발생

--- Spring_basic/basic/src/main/java/basic/ex02/MainClass.java
+++ Spring_basic/basic/src/main/java/basic/ex02/MainClass.java
@@ -1,13 +1,19 @@
 package basic.ex02;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
 	public static void main(String[] args) {
-		MemberDAO mdao = new MemberDAO();
-		DataBaseInfo dbinfo = new DataBaseInfo();
-		mdao.setDbInfo(dbinfo);
-		dbinfo.setUid("test");
-		dbinfo.setUpw("test123");
-		dbinfo.setUrl("com.test");
+//		MemberDAO mdao = new MemberDAO();
+//		DataBaseInfo dbinfo = new DataBaseInfo();
+//		mdao.setDbInfo(dbinfo);
+//		dbinfo.setUid("test");
+//		dbinfo.setUpw("test123");
+//		dbinfo.setUrl("com.test");
+//		mdao.showDBInfo();
+		GenericXmlApplicationContext ct = 
+				new GenericXmlApplicationContext("classpath:db-config.xml");
+		MemberDAO mdao = ct.getBean("mdao", MemberDAO.class);
 		mdao.showDBInfo();
 	}
 }
