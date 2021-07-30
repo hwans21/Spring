dd589a11415932cc1921f4aae74a4238c7f7b9f6
hwans21 <hwans3747@gmail.com>
2021-07-30 11:41:18 +0900

    나머지 기능 작성 및 테스트 진행

--- spring_workspace/SpringWebMvcProject/src/test/java/com/spring/mvc/board/BoardMapperTest.java
+++ spring_workspace/SpringWebMvcProject/src/test/java/com/spring/mvc/board/BoardMapperTest.java
@@ -40,6 +40,41 @@ public class BoardMapperTest {
 		for(BoardVO vo : list) {
 			System.out.println(vo);
 		}
+		System.out.println("게시물 개수: "+ list.size());
+	}
+	
+	//게시글 단일 조회 테스트
+	// 44번글을 조회해서 글 상세 내용을 출력해 주세요
+	@Test
+	public void getContentTest() {
+		BoardVO article = mapper.getArticle(44);
+		System.out.println(article);
+	}
+	
+	
+	//게시글 수정 테스트
+	//BoardVO 객체를 하나 생성하시고, VO의 setter를 사용하여
+	//수정 내용(글 제목, 글 내용)을 입력하고 수정을 진행해 보세요(1번글의 제목, 내용 수정)
+	@Test
+	public void updateTest() {
+		BoardVO article = new BoardVO();
+		article.setBoardNum(1);
+		article.setTitle("제목 수정");
+		article.setContent("내용 수정");
+		mapper.update(article);
+		
+	}
+	
+	
+	
+	//게시글 삭제 테스트
+	//3번글을 삭제하세요 삭제 후 상세보기를 통해 3번글을 가져왔을 때 null이 리턴되는지
+	//조건문으로 확인해서 결과를 출력하세요(null이 왔다면 -> 삭제 성공! 게시물 없음!)
+	@Test
+	public void deleteTest() {
+		mapper.delete(3);
+		if(mapper.getArticle(3)==null)
+			System.out.println("삭제성공! 게시물 없음!");
 	}
 	
 }
