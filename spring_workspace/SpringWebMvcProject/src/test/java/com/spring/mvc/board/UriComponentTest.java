2a5351ed1f9ce24ae83bb7ab92de0acd9cb66626
hwans21 <hwans3747@gmail.com>
2021-08-04 12:59:11 +0900

    테스트 모듈 위치 변경

--- spring_workspace/SpringWebMvcProject/src/test/java/com/spring/mvc/board/UriComponentTest.java
+++ spring_workspace/SpringWebMvcProject/src/test/java/com/spring/mvc/board/UriComponentTest.java
@@ -4,9 +4,9 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.junit.Test;
 
-@Test
+
 public class UriComponentTest {
-	
+	@Test
 	public void testUriComp() {
 		//uri를 쉽게 작성할 수 있도록 도와주는 유틸 클래스
 		//UriComponentsBuilder 사용하기
